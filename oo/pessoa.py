51f575f0b1327c105a186e55f26a1bd8b9314f52
Dácio <daciocoelho@hotmail.com>
2022-02-28 19:22:42 -0300

    Criada a classe pessoa

---
+++ oo/pessoa.py
@@ -0,0 +1,8 @@
+# CLASSE - É A FORMA COMO O OBJETOS SE COMPORTAM
+
+class Pessoa:  #Letras em maisculo no inicio das frases
+    pass
+
+"""[
+A Classe é uma forma de vocÊ criar seues tipos personalizados
+Funciona como forma de gelo, e você precis colocar agua para seer moldada"""
