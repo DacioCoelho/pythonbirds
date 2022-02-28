08cc2a9cd73b4183f9e21f462c1469b6f6495887
Dácio <daciocoelho@hotmail.com>
2022-02-28 20:06:55 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,12 +6,19 @@ Funciona como forma de gelo, e você precis colocar agua para seer moldada
 Metódo é uma função que pertence a uma classe"""
 
 class Pessoa:  #Letras em maisculo no inicio das frases
+    def __init__(self, nome= None, idade= 30):   #Atributo de inicialização
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Ana Cláudia', 23)
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Dácio'
+    print(p.nome)
+    print(p.idade)
 
