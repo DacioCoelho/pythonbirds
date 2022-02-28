3a221ffe724d9bb7711d1d6cd74db27a2b9ad592
Dácio <daciocoelho@hotmail.com>
2022-02-28 19:56:54 -0300

    Criada a classe o metodo cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,17 @@
 # CLASSE - É A FORMA COMO O OBJETOS SE COMPORTAM
+"""
+A Classe é uma forma de vocÊ criar seues tipos personalizados
+Funciona como forma de gelo, e você precis colocar agua para seer moldada
+
+Metódo é uma função que pertence a uma classe"""
 
 class Pessoa:  #Letras em maisculo no inicio das frases
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
 
-"""[
-A Classe é uma forma de vocÊ criar seues tipos personalizados
-Funciona como forma de gelo, e você precis colocar agua para seer moldada"""
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+
