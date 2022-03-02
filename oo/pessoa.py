826e1b451d4dca2f2aa5ad599b84c7e9ec52a20b
Dácio <daciocoelho@hotmail.com>
2022-03-02 10:49:04 -0300

    Criados atributos complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,19 +6,22 @@ Funciona como forma de gelo, e você precis colocar agua para seer moldada
 Metódo é uma função que pertence a uma classe"""
 
 class Pessoa:  #Letras em maisculo no inicio das frases
-    def __init__(self, nome= None, idade= 30):   #Atributo de inicialização
+    def __init__(self, *filhos, nome= None, idade= 30):   #Atributo de inicialização
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Ana Cláudia', 23)
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Dácio'
-    print(p.nome)
-    print(p.idade)
+    bernardo = Pessoa(nome='Bernardo')
+    ana = Pessoa(bernardo, 'Ana Cláudia', 23)
+    print(Pessoa.cumprimentar(ana))
+    print(id(ana))
+    print(ana.cumprimentar())
+    print(ana.idade)
+    print(ana.filhos)
+    for filho in ana.filhos:
+        print(filho.nome)
 
